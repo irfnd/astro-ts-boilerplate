91b7f81ccad3fa5ea308d59168e2d6343cc2dc9b
Irfnd <irfandiabimanyu@gmail.com>
2023-03-25 16:05:53 +0700

    [Add] - Setup react astro page

---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export default function Hello(): React.ReactElement {
+  return (
+    <div>
+      <h1>Welcome to Astro TS Boilerplate</h1>
+      <p>
+        A boilerplate/starter project for quickly building web using Astro with
+        React and TypeScript
+      </p>
+    </div>
+  )
+}
